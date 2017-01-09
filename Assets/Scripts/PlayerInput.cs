6da0bdf56d293ca34183dd2f647d603b72375365
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2017-01-09 02:07:00 -0500

    Added a back yard

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -10,15 +10,35 @@ public class PlayerInput : MonoBehaviour {
     [SerializeField] float speed;
     [SerializeField] GameObject battery;
     [SerializeField] GameObject lightSource;
+    [SerializeField] bool outside;
 
     GameObject interactableObject;
     bool canInteract = false;
+    bool colliding = false;
     int roomNumber = 1;
+    public float currentSpeed;
+
+    void Start() {
+        currentSpeed = speed;
+    }
 
     // Update is called once per frame
     void Update() {
         HandleInput();
         CheckForLight();
+    }
+
+    void OnCollisionStay(Collision other) {
+        
+        if (!other.gameObject.CompareTag("Ground")) {
+            Debug.Log("Colliding with: " + other.gameObject.name);
+            currentSpeed = 1;
+        }
+    }
+
+    void OnCollisionExit(Collision other) {
+        Debug.Log("Not Colliding");
+        currentSpeed = speed;
     }
 
     void CheckForLight() {
@@ -38,24 +58,28 @@ public class PlayerInput : MonoBehaviour {
         if (Input.GetKey(KeyCode.A))
         {
             // move left
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (!outside) { transform.Translate(Vector3.left * currentSpeed * Time.deltaTime); }
+            else { transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime); }
             // transform.Rotate(this.transform.up, -1);
         }
         if (Input.GetKey(KeyCode.D))
         {
             // move right
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            if (!outside) { transform.Translate(Vector3.right * currentSpeed * Time.deltaTime); }
+            else { transform.Translate(Vector3.back * currentSpeed * Time.deltaTime); } 
             //transform.Rotate(this.transform.up, 1);
         }
         if (Input.GetKey(KeyCode.W))
         {
             // move forward
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            if (!outside) { transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime); }
+            else { transform.Translate(Vector3.right * currentSpeed * Time.deltaTime); }
         }
         if (Input.GetKey(KeyCode.S))
         {
             // move back
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            if (!outside) { transform.Translate(Vector3.back * currentSpeed * Time.deltaTime); }
+            else { transform.Translate(Vector3.left * currentSpeed * Time.deltaTime); }
         }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
